6dfc2320ce0dcf9462654b7a3bf8c7d5c37a7593
Tasan <tsmithgandy@gmail.com>
2026-09-25 21:17:26 -0600

    Made Agent class mainly. Sometimes seemingly hung on step() call.

--- for_training/ai_trainer.py
+++ for_training/ai_trainer.py
@@ -2,16 +2,19 @@ import gymnasium as gym
 from gymnasium import spaces
 from ballz_game import *
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, models
 
 class BallzEnv(gym.Env):
     metadata = {"render_modes": ["human"], "render_fps": 1}
     def __init__(self, width, height, nblocks, render_mode):
         self.game = Game(width, height, nblocks)
 
-        # grid dimensions
-        self.dims = (self.game.limit, self.game.nblocks)
+        # grid dimensions with two channels (one for block health and one for tokens)
+        self.dims = (self.game.limit+1, self.game.nblocks, 2)
         self.blocks = np.zeros(self.dims, dtype=int)
-        self.tokens = np.zeros(self.dims, dtype=int)
+        # self.tokens = np.zeros(self.dims, dtype=int)
 
         self.width = width
         self.height = height
@@ -19,7 +22,7 @@ class BallzEnv(gym.Env):
         
         # spaces for observation
         blocks_space = spaces.Box(low=0, high=10000, shape=self.dims, dtype=int)
-        token_space = spaces.Box(low=0, high=1, shape=self.dims, dtype=int)
+        # token_space = spaces.Box(low=0, high=1, shape=self.dims, dtype=int)
         # spaces.MultiBinary(self.dims) 
         nballs_space = spaces.Box(low=0, high=10000, shape=(), dtype=int)
         pos_space = spaces.Box(low=0, high=width, shape=(), dtype=int)
@@ -29,11 +32,11 @@ class BallzEnv(gym.Env):
 
         self.observation_space = gym.spaces.Dict(
             {"blocks": blocks_space,
-             "tokens": token_space,
+             # "tokens": token_space,
              "nballs": nballs_space,
              "position": pos_space
             })
-        self.action_space = gym.spaces.Box(low=-1, high=1, shape=())
+        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(), dtype=np.float32)
 
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
@@ -46,17 +49,18 @@ class BallzEnv(gym.Env):
         tokens = np.zeros(self.dims, dtype=int)
         for block_coord in self.game.blocks:
             block = self.game.blocks[block_coord]
-            blocks[block_coord[::-1]] = int(block.health)
-            tokens[block_coord[::-1]] = int(block.token)
+            blocks[block_coord[1], block_coord[0], 0] = int(block.health)
+            blocks[block_coord[1], block_coord[0], 1] = int(block.token)
+            # tokens[block_coord[::-1]] = int(block.token)
 
         self.blocks = blocks
-        self.tokens = tokens
+        # self.tokens = tokens
         self.nballs = len(self.game.balls)
         self.pos = self.game.launcher.x
             
         observation = {
             "blocks": self.blocks,
-            "tokens": self.tokens,
+            # "tokens": self.tokens,
             "nballs": self.nballs,
             "position": self.pos
         }
@@ -127,3 +131,99 @@ class BallzEnv(gym.Env):
         if self.window is not None:
             pygame.display.quit()
             pygame.quit()
+
+class BallzAgent(models.Model):
+    def __init__(self, env):
+        self.env = env
+        obs_space = env.observation_space
+        self.grid_dims = obs_space["blocks"].shape
+        self.cnn = models.Sequential([
+             layers.Input(self.grid_dims, name='Input'),
+             layers.Conv2D(16, (3,3), padding='same', activation='relu', name='Conv2d Layer 1'),
+             layers.Conv2D(32, (3,3), padding='same', activation='relu', name='Conv2d Layer 2'),
+             layers.Flatten()
+            ])
+
+        self.obs_flat_shape = self.cnn.output_shape[-1] + len(obs_space)-1
+        
+        self.actor = models.Sequential([
+                layers.Input(shape=(self.obs_flat_shape,), name='Input'),
+                layers.Dense(36, activation='relu'),
+                layers.Dense(2, activation='tanh')
+            ])
+
+        self.critic = models.Sequential([
+                layers.Input(shape=(self.obs_flat_shape,), name='Input'),
+                layers.Dense(36, activation='relu'),
+                layers.Dense(1)
+            ])
+
+        self.learning_rate = 1e-3
+        self.cnn_optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
+        self.a_optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
+        self.c_optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
+
+        self.gamma = 0.99
+
+    def forward(self, obs_space):
+        flat_blocks = self.cnn(tf.constant([obs_space["blocks"]]))
+        scalar_tensor = tf.stack([obs_space["nballs"], obs_space["position"]],0)
+        state_tensor = tf.concat([flat_blocks, [scalar_tensor]], 1)
+        return state_tensor
+        
+    def train(self, n_episodes=1000):
+        rewards = []
+        for _ in range(n_episodes):
+            state, info = self.env.reset()
+            state_tensor = self.forward(state)
+            episode_reward, new_state = self.run_episode(state_tensor)
+
+
+        return rewards
+
+    def run_episode(self, state_tensor):
+        terminated = False
+        tot_reward = 0
+        while not terminated:
+            with tf.GradientTape() as actor_tape, tf.GradientTape() as critic_tape, tf.GradientTape() as cnn_tape:
+                # Actor mean and std prediction assuming gaussian distribution
+                a_mean, a_std = self.actor(state_tensor)[0]
+                print("m,std: ", a_mean, a_std)
+                action = np.clip(np.random.normal(a_mean, np.abs(a_std)), a_min=-0.9, a_max=0.9) # sample from gaussian
+                print("action: ", action)
+                observation, reward, terminated, _, info = self.env.step(action)
+                print("reward: ", reward)
+                new_state_tensor = self.forward(observation) # observation as a tensor
+
+                curr_val = self.critic(state_tensor)[0][0]
+                next_val = self.critic(new_state_tensor)[0][0]
+                print(curr_val, next_val+reward)
+
+                if terminated:
+                    # no next state exists
+                    target = reward
+                else:
+                    # what the predicted reward actually is given next state
+                    target = reward + self.gamma*next_val
+
+                advantage = target - curr_val
+
+                # log of the probability density for a gaussian
+                log_pi = lambda x: tf.math.log(1/(a_std*np.sqrt(2*np.pi))) - tf.math.square(x - a_mean)/(2*tf.math.square(a_std)) 
+                actor_loss = log_pi(action) * tf.stop_gradient(advantage)
+                critic_loss = tf.math.square(advantage)
+
+            # use critic loss to update cnn featureinator
+            cnn_gradient = cnn_tape.gradient(critic_loss, self.cnn.trainable_weights)
+            self.cnn_optimizer.apply_gradients(zip(cnn_gradient, self.cnn.trainable_weights))
+            # update critic and actor
+            c_gradient = critic_tape.gradient(critic_loss, self.critic.trainable_weights)
+            self.c_optimizer.apply_gradients(zip(c_gradient, self.critic.trainable_weights))
+            a_gradient = actor_tape.gradient(actor_loss, self.actor.trainable_weights)
+            self.a_optimizer.apply_gradients(zip(a_gradient, self.actor.trainable_weights))
+
+            print(reward,terminated)
+            state_tensor = new_state_tensor
+            tot_reward += reward
+
+        return tot_reward, state_tensor 
